e0ec9f54964a0ed3c2ca172c8ca37e194be6f692
Emmanuel Donkor <edonkor0000@gmail.com>
2022-12-25 18:39:06 +0100

    second commit

--- src/App.js
+++ src/App.js
@@ -2,20 +2,60 @@ import { useState } from "react";
 import "./App.css";
 
 const App = () => {
-const[userName, setUserName]= useState("Emmanuel")
+  const [todo, setTodo] = useState({
+    userName: "Emmanuel",
+    todoItems: [
+      { task: "Buy Flowers", done: false },
+      { task: "Get Grocery", done: false },
+      { task: "Get Jacket", done: true },
+      { task: "Get Shirt", done: false },
+    ],
+    newItemText: "",
+  });
 
-const changeDataState=()=>{
-     setUserName((prevUserName)=>(
-      prevUserName === "Emmanuel" ? "Donkor" : "Adom"
-     ))
-}
+  const changeDataState = () => {
+    setTodo((prevTodo) => ({
+      ...prevTodo,
+      userName: prevTodo.userName === "Emmanuel" ? "Adom" : "Donkor",
+    }));
+  };
+
+  const updateNewTextValue = (event) => {
+    setTodo((prevTodo) => ({ ...prevTodo, newItemText: event.target.value }));
+  };
+  const createNewTodo = () => {
+    if (!todo.todoItems.find((item) => item.action === todo.newItemText)) {
+      setTodo((prevTodo) => ({
+        ...prevTodo,
+        todoItems: [
+          ...prevTodo.todoItems,
+          { action: prevTodo.newItemText, done: false },
+        ],
+        newItemText: "",
+      }));
+    }
+  };
   return (
     <div className="App">
       <h4 className="bg-primary text-white text-center p-2">
-       {userName}'s Todo List
+        {todo.userName}'s To Do List (
+        {todo.todoItems.filter((t) => !t.done).length} items to do)
       </h4>
-      <button className="btn btn-primary m-2"
-      onClick={changeDataState}>Change</button>
+      <button className="btn btn-primary m-2" onClick={changeDataState}>
+        Change
+      </button>
+      <div className="container-fuild">
+        <div className="my-1">
+          <input
+            className="form-control"
+            value={todo.newItemText}
+            onChange={updateNewTextValue}
+          />
+          <button className="btn btn-primary mt-1" onClick={createNewTodo}>
+            Add
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
---
+++ src/components/TodoBanner/TodoBanner.js
@@ -0,0 +1,8 @@
+
+
+const TodoBanner = () =>{
+    return (
+        <h1>Yes yall</h1>
+    )
+}
+export default TodoBanner
